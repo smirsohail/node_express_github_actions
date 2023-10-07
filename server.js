c564a6c139e66f37c1a605c6c350e84f656083fe
mirsohailshaikh <mirsohail.shaikh@bellmedia.ca>
2023-10-07 12:07:41 -0400

    adding nav

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const SERVER_HOST = "localhost"
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-app.use(expres.static("./views"))
+app.use(express.static("./views"))
 
 //http://localhost:3000/
 app.get('/', function (req, res) {
